e1ebb1b618b6c05ef9ac80b7481a294d4de3941c
Ruslan <rus.ilin1979@gmail.com>
2023-04-15 22:08:16 +0500

    ДЗ-2

---
+++ 2/task-2.js
@@ -0,0 +1,104 @@
+let goods = [
+    {
+        id: 1,
+        name: "Кроссовки",
+        description: "text",
+        sizes: [38, 39, 40],
+        price: 3000,
+        available: true,
+    },
+    {
+        id: 33,
+        name: "Туфли",
+        description: "text",
+        sizes: [35, 36, 37, 38, 40],
+        price: 4000,
+        available: false,
+    },
+    {
+        id: 56,
+        name: "Тапочки",
+        description: "text",
+        sizes: [38, 39, 42],
+        price: 700,
+        available: true,
+    },
+    {
+        id: 127,
+        name: "Ботинки",
+        description: "text",
+        sizes: [37, 39, 40],
+        price: 5000,
+        available: true,
+    },
+    {
+        id: 876,
+        name: "Сапоги женские",
+        description: "text",
+        sizes: [36, 39],
+        price: 7500,
+        available: true,
+    },
+
+];
+
+let shoppingСart = [
+
+];
+
+
+shoppingСart.addGood = function (goodIndex, amount) {
+    this.push({
+        good: goodIndex,
+        amount,
+    })
+}
+
+shoppingСart.removeGood = function (goodIndex, amount) {
+    for (let i = 0; i < this.length; ++i) {
+        if (this[i].good == goodIndex) {
+            if (amount >= this[i].amount) {
+                this.splice(i, 1);
+                return;
+            }
+
+            this[i].amount -= amount;
+        }
+    }
+}
+
+shoppingСart.clear = function () {
+    this.splice(0, this.length);
+}
+
+shoppingСart.getTotalGoods = function (goods) {
+    result = {
+        totalAmount: 0,
+        totalSumm: 0,
+    }
+
+    for (let el of this) {
+        result.totalAmount += el.amount;
+        result.totalSumm += goods[el.good].price * el.amount;
+    }
+
+    return result;
+}
+
+
+function main(shoppingСart, goods) {
+    shoppingСart.addGood(0, 10);
+    shoppingСart.removeGood(0, 5);
+    shoppingСart.addGood(1, 1);
+    shoppingСart.addGood(2, 7);
+    shoppingСart.addGood(3, 1);
+    shoppingСart.removeGood(3, 1);
+
+    console.log(shoppingСart)
+    console.log(shoppingСart.getTotalGoods(goods));
+    shoppingСart.clear();
+    console.log(shoppingСart.getTotalGoods(goods));
+}
+
+
+main(shoppingСart, goods);
